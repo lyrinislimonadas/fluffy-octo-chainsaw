f120fc027f773a96d3466f2e991da6588155a5e3
lyrinislimonadas <gintaras.gruodis@stud.svako.lt>
2026-01-16 23:23:20 +0200

    Pilot_version
    Pilot version upload

---
+++ phpCiklaiGintarasGruodisIIST23.php
@@ -0,0 +1,20 @@
+<?php
+// Pradinė reikšmė
+$x = 7; // gali pakeisti į bet kokį skaičių
+$iteracijos = 0;
+echo "Pradžia: $x\n";
+// Kartojame, kol reikšmė taps 1
+while ($x != 1) {
+    if ($x % 2 == 0) {
+        // Lyginis skaičius – daliname iš 2
+        $x = $x / 2;
+    } else {
+        // Nelyginis skaičius – skaičiuojame 3x + 1
+        $x = 3 * $x + 1;
+    }
+    $iteracijos++;
+    echo "Reikšmė: $x\n";
+}
+echo "Iteracijų skaičius: $iteracijos\n";
+?>
+
